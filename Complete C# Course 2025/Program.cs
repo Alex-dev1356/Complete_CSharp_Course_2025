5e18450c31c1392c5c8b3864acc36a0000f8073f
Alex-dev1356 <asoliven26@yahoo.com>
2025-05-10 23:35:41 +0800

    Creating the solution file for Complete C# Course

---
+++ Complete C# Course 2025/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Complete_C__Course_2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
