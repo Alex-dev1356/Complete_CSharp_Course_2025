fa4087dabf557493bcafa4426c140fd71bdd4bdb
Alex-dev1356 <asoliven26@yahoo.com>
2025-05-11 00:34:26 +0800

    Cretation of Switch Expression Project

---
+++ Switch Expressions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Switch_Expressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
