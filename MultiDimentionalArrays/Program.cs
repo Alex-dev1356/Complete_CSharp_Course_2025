81e3713a4b62efbafb32417fc956d8931bafe0bc
Alex-dev1356 <asoliven26@yahoo.com>
2025-05-18 16:28:46 +0800

    Creation of MultiDimensionalArrays Project

---
+++ MultiDimentionalArrays/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MultiDimentionalArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] letters = new char[2, 3];
+            
+            letters[0, 0] = 'a';
+            letters[0, 1] = 'b';
+            letters[0, 2] = 'c';
+            letters[1, 0] = 'd';
+            letters[1, 1] = 'e';
+            letters[1, 2] = 'f';
+        }
+    }
+}
