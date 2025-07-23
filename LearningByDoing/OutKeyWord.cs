2b49d03ffa1bba460eb02920a07f7549dc0cef9c
Alex-dev1356 <asoliven26@yahoo.com>
2025-07-24 05:13:07 +0800

    Recalling the execution of Out Key Word on my own to better understand it

---
+++ LearningByDoing/OutKeyWord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningByDoing
+{
+    public class OutKeyWord
+    {
+        //Recalling the execution of the Out Keyword example from the lecture.
+        
+        public List<int> GetOnlyPositiveNumbers(
+            int[] numbers, out List<int> numberOfNonPositiveNumbers)
+        {
+           numberOfNonPositiveNumbers = new List<int>();
+
+            List<int> result = new List<int>();
+
+            foreach (int number in numbers)
+            {
+                if (number > 0)
+                {
+                    result.Add(number);
+                }
+                else
+                {
+                    numberOfNonPositiveNumbers.Add(number);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LearningByDoing/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LearningByDoing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Out Keyword execution.
+
+            //Declaration of an array of integers.
+            int[] numbers = new int[] { 10, -20, 30, -40, 50 };
+
+            //Calling the method with the OutKeyword Class.
+            var outKeyWord = new OutKeyWord();
+            var result = outKeyWord.GetOnlyPositiveNumbers(numbers, out List<int> nonPositiveNumbers);
+
+            //Printing the Positive Numbers and the count of Non-Positive Numbers
+            foreach (int number in result)
+            {
+                Console.WriteLine($"Positive Number: {number}");
+            }
+
+            //Console.WriteLine($"Number of Non-Positive Numbers: {nonPositiveNumbersCount}");
+
+
+            //Printing the Non-Positive Numbers
+            foreach (int nonPositive in nonPositiveNumbers)
+            {
+                Console.WriteLine($"Non-Positive Number: {nonPositive}");
+            }
+        }
+    }
+}
