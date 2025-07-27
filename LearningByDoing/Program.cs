2ecbaaff276500da93436edaf1da079e34b255ea
Alex-dev1356 <asoliven26@yahoo.com>
2025-07-27 12:20:56 +0800

    Recalling the execution of TryParse Method on my own to better understand it

--- LearningByDoing/Program.cs
+++ LearningByDoing/Program.cs
@@ -5,28 +5,44 @@
         static void Main(string[] args)
         {
             //Out Keyword execution.
+            #region
+            ////Declaration of an array of integers.
+            //int[] numbers = new int[] { 10, -20, 30, -40, 50 };
 
-            //Declaration of an array of integers.
-            int[] numbers = new int[] { 10, -20, 30, -40, 50 };
+            ////Calling the method with the OutKeyword Class.
+            //var outKeyWord = new OutKeyWord();
+            //var result = outKeyWord.GetOnlyPositiveNumbers(numbers, out List<int> nonPositiveNumbers);
 
-            //Calling the method with the OutKeyword Class.
-            var outKeyWord = new OutKeyWord();
-            var result = outKeyWord.GetOnlyPositiveNumbers(numbers, out List<int> nonPositiveNumbers);
+            ////Printing the Positive Numbers and the count of Non-Positive Numbers
+            //foreach (int number in result)
+            //{
+            //    Console.WriteLine($"Positive Number: {number}");
+            //}
 
-            //Printing the Positive Numbers and the count of Non-Positive Numbers
-            foreach (int number in result)
+            ////Console.WriteLine($"Number of Non-Positive Numbers: {nonPositiveNumbersCount}");
+
+
+            ////Printing the Non-Positive Numbers
+            //foreach (int nonPositive in nonPositiveNumbers)
+            //{
+            //    Console.WriteLine($"Non-Positive Number: {nonPositive}");
+            //}
+            #endregion
+
+            //TryParseMethod execution
+            #region
+            var tryParse = TryParseMethod.TryParseMethodExecution();
+            if (tryParse > 0)
             {
-                Console.WriteLine($"Positive Number: {number}");
+                Console.WriteLine($"Parsing successful. Result: {tryParse}");
             }
-
-            //Console.WriteLine($"Number of Non-Positive Numbers: {nonPositiveNumbersCount}");
-
-
-            //Printing the Non-Positive Numbers
-            foreach (int nonPositive in nonPositiveNumbers)
+            else
             {
-                Console.WriteLine($"Non-Positive Number: {nonPositive}");
+                //By Default if the TryParse Method fails the result will be 0
+                Console.WriteLine($"tryParse value if failed parsing: {tryParse}");
+                Console.WriteLine("Parsing was not successful or the result is zero.");
             }
+            #endregion
         }
     }
 }
---
+++ LearningByDoing/TryParseMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningByDoing
+{
+    public class TryParseMethod
+    {
+        public static int TryParseMethodExecution()
+        {
+            Console.WriteLine("Enter a number: ");
+            var userInput = Console.ReadLine();
+            bool isParsingSuccessful = int.TryParse(userInput, out int result);
+
+            return result;
+        }
+    }
+}
