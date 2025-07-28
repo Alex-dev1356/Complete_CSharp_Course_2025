f882742b049cd6d1823bcb7f9f365d40ed10ce4d
Alex-dev1356 <asoliven26@yahoo.com>
2025-07-28 23:42:53 +0800

    OOP_Abstraction and Data Hiding using Access Modifiers

---
+++ OOP_Abstraction/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace OOP_Abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Abstraction means that classes expose only essential data and methods and hide
+            //the underlying details.
+
+            var rectangle1 = new Rectangle(5,10);
+        }
+    }
+}
---
+++ OOP_Abstraction/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Abstraction
+{
+    public class Rectangle
+    {
+        //Declariung fields
+        //A field is a variable that belongs to an object of a class.
+        ///Each instance of the rectangle class can have different values of those fields.
+        //So, for example, we can have one rectangle instance of width ten and height two,
+        //and another of width five and height six.
+        public int _width;
+        public int _height;
+
+        //Declaring a constructor
+        public Rectangle(int width, int height)
+        {
+             _width = width;
+             _height = height;
+        }
+
+
+        
+    }
+}
