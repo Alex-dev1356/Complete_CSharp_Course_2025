883cf22c797609ed96aa8a31c73a209abb466b8d
Alex-dev1356 <asoliven26@yahoo.com>
2025-07-28 16:39:55 +0800

    OOP_DateTimeType

---
+++ ObjectOrientedProgramming/OOP_DateTimeType.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramming
+{
+    public class OOP_DateTimeType
+    {
+        //public OOP_DateTimeType()
+        //{
+        //   
+        //    DateTime internationalPizzaDay2023;
+        //}
+
+        public DateTime DateTime()
+        {
+           /*
+           Constructor used specifically to create new class instances. We'll learn more about constructors later in the course.
+           In this case, the constructor takes three integer parameters: the year, month, and the day of the date
+           we want to represent. We could also pass more parameters representing hours, minutes, seconds,etc.
+           */
+            var internationalPizzaDay2023 = new DateTime(2023, 2, 9); 
+
+            return internationalPizzaDay2023;
+        }
+
+        //Add Years Method
+        public DateTime AddYears(DateTime date, int years)
+        {
+            var newDate = date.AddYears(years);
+            return newDate;
+        }
+        
+    }
+}
---
+++ ObjectOrientedProgramming/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ObjectOrientedProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //OOP_DateTimeType
+            var dateTimeType = new OOP_DateTimeType();
+            var internationalPizzaDay2023 =  new DateTime(2023, 2, 9);
+            var differentCapabilitiesOfDateTimeType = dateTimeType.DateTime();
+
+            Console.WriteLine("year is " + differentCapabilitiesOfDateTimeType.Year);
+            Console.WriteLine("month is " + differentCapabilitiesOfDateTimeType.Month);
+            Console.WriteLine("day is " + differentCapabilitiesOfDateTimeType.Day);
+            //This is where the true power of object comes in. If we dont have this function
+            //we would need to compute the day of the week from scratch.
+            Console.WriteLine("day of the week is " + differentCapabilitiesOfDateTimeType.DayOfWeek);
+
+            //Add Years Method
+            var internationalPizzaDay2024 = dateTimeType.AddYears(internationalPizzaDay2023, 1);
+            Console.WriteLine();
+            Console.WriteLine("year is " + internationalPizzaDay2024.Year);
+            Console.WriteLine("month is " + internationalPizzaDay2024.Month);
+            Console.WriteLine("day is " + internationalPizzaDay2024.Day);
+            Console.WriteLine("day of the week is " + internationalPizzaDay2024.DayOfWeek);
+        }
+    }
+}
