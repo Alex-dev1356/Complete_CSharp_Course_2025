a341351fe52b18ba93de8b92bf0902b4fec2d99e
Alex-dev1356 <asoliven26@yahoo.com>
2025-08-04 10:09:48 +0800

    Constructor Overloading using the 'this' keyword

---
+++ Constructor_Overloading/MedicalAppointment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Overloading
+{
+    public class MedicalAppointment
+    {
+        private string _patientName;
+        private DateTime _date;
+
+        public MedicalAppointment(string patientName, DateTime date)
+        {
+            _patientName = patientName;
+            _date = date;
+        }
+
+        public MedicalAppointment(string patientName, int daysFormNow)
+        {
+            _patientName = patientName;
+            _date = DateTime.Now.AddDays(daysFormNow);
+        }
+
+        //public MedicalAppointment(string patientName)
+        //{
+        //    _patientName = patientName;
+        //    _date = DateTime.Now.AddDays(7);
+        //}
+
+        //Using the 'this' keyword to call one constructor from another constructor.
+        public MedicalAppointment(string patientName) : 
+            this(patientName, 7)
+        {
+        }
+
+        public void Rechedule(DateTime date)
+        {
+            _date = date;
+        }
+
+
+    }
+}
---
+++ Constructor_Overloading/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Constructor_Overloading
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Constructor Overloading - Calling one constructor from another using the 'this' keywordr
+
+
+        }
+    }
+}
