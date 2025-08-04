d34547842679a86f89cdbbfc3d3c3dd0dd9d26c5
Alex-dev1356 <asoliven26@yahoo.com>
2025-08-04 10:09:08 +0800

    Ignored Files

--- Constructor_Overloading/obj/Debug/net8.0/Constructor_Overloading.AssemblyInfo.cs
+++ Constructor_Overloading/obj/Debug/net8.0/Constructor_Overloading.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constructor_Overloading")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be6e4394d866756fbb97a545363459b25da52518")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aee95fdfd5be4fc327b1496bba6093db2d96ed0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constructor_Overloading")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constructor_Overloading")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
