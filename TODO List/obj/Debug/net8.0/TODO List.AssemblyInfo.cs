1359d29589a8f4005b16143ff07ad8a83a8cc267
Alex-dev1356 <asoliven26@yahoo.com>
2025-08-05 22:22:31 +0800

    Miscelaneous files for new project creation

--- TODO List/obj/Debug/net8.0/TODO List.AssemblyInfo.cs
+++ TODO List/obj/Debug/net8.0/TODO List.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TODO List")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4c1b9f541ce94a7f7417300043bd9a5f8c1527a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ebeae9c82f147f6454e54ade433793fdf753b8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TODO List")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TODO List")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
