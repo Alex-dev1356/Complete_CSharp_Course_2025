e16bd1e340385f594719d9eb3607a66c0ad2df90
Alex-dev1356 <asoliven26@yahoo.com>
2025-08-05 22:16:32 +0800

    Ignore Miscelaneous Files

---
+++ Optional_Parameters/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Optional_Parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- TryParseMethod/obj/Debug/net8.0/TryParseMethod.AssemblyInfo.cs
+++ TryParseMethod/obj/Debug/net8.0/TryParseMethod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TryParseMethod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4c1b9f541ce94a7f7417300043bd9a5f8c1527a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d18c146c4e7cdfc11d6d8bad21dcc05e1f243703")]
 [assembly: System.Reflection.AssemblyProductAttribute("TryParseMethod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TryParseMethod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
