f4084f4498b756b1a663967bdef4e664410f8d88
Alex-dev1356 <asoliven26@yahoo.com>
2025-08-25 10:45:59 +0800

    Miscelaneous Files for the new Project Creation

--- CSharpFundamentals/obj/Debug/net8.0/CSharpFundamentals.AssemblyInfo.cs
+++ CSharpFundamentals/obj/Debug/net8.0/CSharpFundamentals.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpFundamentals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+751d26ecd455819011c9dcda71880094a359b577")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3efdfb038086b9d884f343f8248ef4dd4dfcd3d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpFundamentals")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpFundamentals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Validation_Of_Constructor_Parameter/obj/Debug/net8.0/Validation_Of_Constructor_Parameter.AssemblyInfo.cs
+++ Validation_Of_Constructor_Parameter/obj/Debug/net8.0/Validation_Of_Constructor_Parameter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Validation_Of_Constructor_Parameter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+751d26ecd455819011c9dcda71880094a359b577")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3efdfb038086b9d884f343f8248ef4dd4dfcd3d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Validation_Of_Constructor_Parameter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Validation_Of_Constructor_Parameter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
