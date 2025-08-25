2420774a9540458a08610262f9bcbad2c043a898
Alex-dev1356 <asoliven26@yahoo.com>
2025-08-25 10:46:27 +0800

    Name Before SRP Refactoring

---
+++ NamesBeforeRefactorToSRP/NameBeforeSRP.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamesBeforeRefactorToSRP
+{
+    public class NameBeforeSRP
+    {
+        private readonly List<string> _names = new List<string>();
+
+        public void AddName(string name)
+        {
+            if (IsValidName(name))
+            {
+                _names.Add(name);
+            }
+        }
+
+        private bool IsValidName(string name)
+        {
+            return
+                name.Length >= 2 &&
+                name.Length < 25 &&
+                char.IsUpper(name[0]) &&
+                name.All(char.IsLetter);
+        }
+
+        public void ReadFromTextFile()
+        {
+            var fileContents = File.ReadAllText(BuildFilePath());
+            var namesFromFile = fileContents.Split(Environment.NewLine).ToList();
+            foreach (var name in namesFromFile)
+            {
+                AddName(name);
+            }
+        }
+
+        public void WriteToTextFile() =>
+            File.WriteAllText(BuildFilePath(), Format());
+
+        public string BuildFilePath()
+        {
+            //we could imagine this is much more complicated
+            //for example that path is provided by the user and validated
+            return "names.txt";
+        }
+
+        public string Format() =>
+            string.Join(Environment.NewLine, _names);
+    }
+}
---
+++ NamesBeforeRefactorToSRP/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace NamesBeforeRefactorToSRP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new NameBeforeSRP();
+            var path = names.BuildFilePath();
+
+            if (File.Exists(path))
+            {
+                Console.WriteLine("Names file already exists. Loading names.");
+                names.ReadFromTextFile();
+            }
+            else
+            {
+                Console.WriteLine("Names file does not yet exist.");
+
+                //let's imagine they are given by the user
+                names.AddName("John");
+                names.AddName("not a valid name");
+                names.AddName("Claire");
+                names.AddName("123 definitely not a valid name");
+
+                Console.WriteLine("Saving names to the file.");
+                names.WriteToTextFile();
+            }
+            Console.WriteLine(names.Format());
+
+            Console.ReadLine();
+        }
+    }
+}
