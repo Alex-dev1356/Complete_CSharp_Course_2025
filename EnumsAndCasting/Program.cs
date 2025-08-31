4a67b8afe10c045f473caad40f3e957a0800940d
Alex-dev1356 <asoliven26@yahoo.com>
2025-08-31 09:54:37 +0800

    Enums and Casting

---
+++ EnumsAndCasting/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace EnumsAndCasting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Days days = Days.Monday;
+            Console.WriteLine(days);
+
+            //Casting - converting one type to another
+            int day = (int)days;
+            Console.WriteLine(day);
+
+            Days day1 = (Days)2;
+            Console.WriteLine(day1);
+
+            var day2 = (Days)8;
+            Console.WriteLine(day2);
+        }
+    }
+
+    public enum Days
+    {
+        Monday, //0
+        Tuesday, //1
+        Wednesday, //2
+        Thursday, //3
+        Friday, //4
+        Saturday, //5
+        Sunday //6
+    }
+}
