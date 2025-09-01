c3b6a75ed2bd69d3b1bd5d4fc7dc687bcddb4415
Alex-dev1356 <asoliven26@yahoo.com>
2025-09-01 10:34:37 +0800

    Extenstion Methods

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ExtensionMethods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "Hello Extension Methods";
+            int wordCount = ExtensionMethodBeforeCSharp14.WordCount(inputString);
+            Console.WriteLine();
+
+        }
+    }
+
+    //Declaring an extension Method before C# 14
+    public static class ExtensionMethodBeforeCSharp14
+    {
+        public static int WordCount(this string str) =>
+            str.Split([' ', '.', '?'], StringSplitOptions.RemoveEmptyEntries).Length;
+    }
+
+    //public static class MyExtensions
+    //{
+    //    extension(string str)
+    //    {
+    //        public int WordCount() =>
+    //            str.Split([' ', '.', '?'], StringSplitOptions.RemoveEmptyEntries).Length;
+    //    }
+    //}
+}
