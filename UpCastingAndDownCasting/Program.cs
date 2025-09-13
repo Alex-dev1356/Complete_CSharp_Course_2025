55cd4374c5b7e62e9cbfe02e177332332658b596
Alex-dev1356 <asoliven26@yahoo.com>
2025-09-13 17:14:00 +0800

    Up Casting and Down Casting

---
+++ UpCastingAndDownCasting/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace UpCastingAndDownCasting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person() { Name = "Alex" };
+
+            var student = new Student() { StudentId = 1, Name = "Allen" };
+
+            //UpCasting - Casting from a Derived class to a Base class
+            Person upCastedPerson = student as Person; // This will assign student to upCastedPerson since Student is a Person
+
+
+            //DownCasting - Casting from a Base class to a Derived class
+            Student? downCastedStudent = person as Student; // Casting the person to a derived class Student
+                                                            // This will assign null to downCastedStudent since person is not a Student
+
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+
+    public class Student : Person
+    {
+        public int StudentId { get; set; }
+    }
+}
