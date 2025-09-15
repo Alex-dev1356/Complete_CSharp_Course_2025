b08a77bd1bc33f0a345fb39890cf1af276a732b3
Alex-dev1356 <asoliven26@yahoo.com>
2025-09-15 18:59:14 +0800

    Abstract Classes

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Abstract Class - a class that cannot be instantiated. They only serve as a base class for other, more conrete types.
+
+            //Abstract classes can also define Abstract methods and properties. 
+        }
+    }
+
+    //Declaring an abstract class
+    public abstract class Ingredient
+    {
+        protected Ingredient(int priceIfExtraTopping)
+        {
+            Console.WriteLine(
+                "Constructor from the Ingredient class.");
+            PriceIfExtraTopping = priceIfExtraTopping;
+        }
+
+        public int PriceIfExtraTopping { get; }
+
+        public override string ToString() => Name;
+
+        public virtual string Name { get; } = "Sone ingredient";
+    }
+
+
+}
