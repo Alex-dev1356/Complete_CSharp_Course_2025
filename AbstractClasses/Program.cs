f3ed84e213157cc6f2ae85347f048284b939c1ba
Alex-dev1356 <asoliven26@yahoo.com>
2025-09-16 04:52:26 +0800

    Abstract Methods and Properties

--- AbstractClasses/Program.cs
+++ AbstractClasses/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AbstractClasses
+﻿using System.Threading.Channels;
+
+namespace AbstractClasses
 {
     internal class Program
     {
@@ -7,6 +9,15 @@
             //Abstract Class - a class that cannot be instantiated. They only serve as a base class for other, more conrete types.
 
             //Abstract classes can also define Abstract methods and properties. 
+
+            //Virtual methods are methods that can be overridden in derived classes. 
+
+            //Abstract methods are methods that must be overridden in derived classes. They can only be defined in abstract classes.
+            //and they don't have implementations.
+
+            //NOTE: All Abstract methods are implicitly virtual.They are obligatory to override them in the non-abstract derived classes.
+
+
         }
     }
 
@@ -25,7 +36,41 @@
         public override string ToString() => Name;
 
         public virtual string Name { get; } = "Sone ingredient";
+
+        //Declaring an abstract method
+        public abstract void Prepare();
     }
 
+    //Creating classes that Inherit from an abstract class
+    public abstract class Cheese : Ingredient
+    {
+        public Cheese(int priceIfExtraTopping) : base(priceIfExtraTopping)
+        {
+        }
 
+        //The only way to skip an abstract method from the base class is if the derived class is abstract itself.
+    }
+
+    public class Cheddar : Cheese
+    {
+        public Cheddar(int priceIfExtraTopping) : base(priceIfExtraTopping)
+        {
+            
+        }
+        //Overriding the abstract method from the Ingredient base class
+        public override void Prepare() =>
+            Console.WriteLine("Grating and sprinkle over the pizza."); ;
+        
+    }
+
+    public class Mozzarella : Cheese
+    {
+        public Mozzarella(int priceIfExtraTopping) : base(priceIfExtraTopping)
+        {
+        }
+
+        //Overriding the abstract method from the Ingredient base class
+        public override void Prepare() => 
+            Console.WriteLine("Slice thinly and place on top of the pizza.");
+    }
 }
